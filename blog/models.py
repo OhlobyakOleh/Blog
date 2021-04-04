abeb35875cafbabcf4ae08ab4bcc970f1b4a5e00
OhlobyakOleh <81420194+OhlobyakOleh@users.noreply.github.com>
2021-04-04 13:55:52 +0300

    commit

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_lenght=200)
+    title = models.CharField(max_length=200)
     autor = models.ForeignKey(
-        'auth.User',
+        User,
         on_delete=models.CASCADE,
     )
     body = models. TextField()
